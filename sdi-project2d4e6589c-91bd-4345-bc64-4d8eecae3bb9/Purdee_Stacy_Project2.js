d2a5e4a6fd5178e8a9d493e50e2e1c0a23b171ad
Stacy Purdee <stacy.purdee@gmail.com>
2013-10-10 20:48:59 -0400

    initial setup

---
+++ sdi-project2d4e6589c-91bd-4345-bc64-4d8eecae3bb9/Purdee_Stacy_Project2.js
@@ -0,0 +1,28 @@
+//alert("JavaScript works!");
+
+/*
+Stacy Purdee
+October 10th 2013
+Project 2
+Story with a flowchart
+*/
+
+//Global Variables-string, number, boolean
+
+//Prompt
+
+//Procedure
+
+//Confirm
+
+//Boolean Function
+
+//Number Function
+
+//String Function
+
+//Returned Values
+
+//Output
+
+//Finish
